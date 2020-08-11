b72a8182da3630db7f7fd5b70e1ededafdd32181
Pedro Araújo Júnior <pedroaraujojunior@gmail.com>
2020-08-11 20:22:46 -0300

    Identação e condição ao imprimir o resultado

--- DesafioEngineering/src/Main.java
+++ DesafioEngineering/src/Main.java
@@ -6,55 +6,57 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	//========= TESTE 1 =========//	
-	int[] a = {1, 2, 3};
-	int[] b = {3, 4};
-	int[][] query = {{1,5}, {0,0,1}, {1,5}};
-	
-	int[] resultados = processaQuery(a, b, query);
-	imprimirResultados(resultados);
-	
-	//========= TESTE 2 =========//	
-	a = new int[]{1, 2, 2};
-	b = new int[]{2, 3};
-	query = new int[][]{{1,4}, {0,0,3}, {1,5}};
-	
-	resultados = processaQuery(a, b, query);
-	imprimirResultados(resultados);
-	
-	//========= TESTE 3 =========//	
-	a = new int[]{1};
-	b = new int[]{2};
-	query = new int[][]{{1,2}};
-	
-	resultados = processaQuery(a, b, query);
-	imprimirResultados(resultados);
-	
-	//========= TESTE 4 =========//
-	a = new int[]{1, 1};
-	b = new int[]{3, 3, 3};
-	query = new int[][]{{0, 1, 1}, {1, 0}, {1, 2}, {1, 1}};
-	
-	resultados = processaQuery(a, b, query);
-	imprimirResultados(resultados);	
-	
-	//========= TESTE 5 =========//
-	a = new int[]{5, 6, 4, 4, 10, 6};
-	b = new int[]{8, 3, 0};
-	query = new int[][]{{1, 12}, {1, 2}, {1, 4}, {1, 1}, {1, 12}};
-	
-	resultados = processaQuery(a, b, query);
-	imprimirResultados(resultados);	
+		//========= TESTE 1 =========//	
+		int[] a = {1, 2, 3};
+		int[] b = {3, 4};
+		int[][] query = {{1,5}, {0,0,1}, {1,5}};
+		
+		int[] resultados = processaQuery(a, b, query);
+		imprimirResultados(resultados);
+		
+		//========= TESTE 2 =========//	
+		a = new int[]{1, 2, 2};
+		b = new int[]{2, 3};
+		query = new int[][]{{1,4}, {0,0,3}, {1,5}};
+		
+		resultados = processaQuery(a, b, query);
+		imprimirResultados(resultados);
+		
+		//========= TESTE 3 =========//	
+		a = new int[]{1};
+		b = new int[]{2};
+		query = new int[][]{{1,2}};
+		
+		resultados = processaQuery(a, b, query);
+		imprimirResultados(resultados);
+		
+		//========= TESTE 4 =========//
+		a = new int[]{1, 1};
+		b = new int[]{3, 3, 3};
+		query = new int[][]{{0, 1, 1}, {1, 0}, {1, 2}, {1, 1}};
+		
+		resultados = processaQuery(a, b, query);
+		imprimirResultados(resultados);	
+		
+		//========= TESTE 5 =========//
+		a = new int[]{5, 6, 4, 4, 10, 6};
+		b = new int[]{8, 3, 0};
+		query = new int[][]{{1, 12}, {1, 2}, {1, 4}, {1, 1}, {1, 12}};
+		
+		resultados = processaQuery(a, b, query);
+		imprimirResultados(resultados);	
 	
 	}
 
 	private static void imprimirResultados(int[] resultados) {
-		System.out.print("[");
-		System.out.print(resultados[0]);
-		for (int i = 1; i < resultados.length; i++) {
-			System.out.print(", "+resultados[i]);
+		if(resultados.length > 0) {
+			System.out.print("[");
+			System.out.print(resultados[0]);
+			for (int i = 1; i < resultados.length; i++) {
+				System.out.print(", "+resultados[i]);
+			}
+			System.out.print("]\n");
 		}
-		System.out.print("]\n");
 	}
 
 	private static int[] processaQuery(int[] a, int[] b, int[][] query) {
@@ -84,7 +86,6 @@ public class Main {
 			} else {
 				System.out.println("Query inválida");
 			}
-			
 		}
 		
 		int[] retorno = new int[resultados.size()];
@@ -92,11 +93,6 @@ public class Main {
 		
 		return retorno;
 		
-		
-		
 	}
-	
-	
-	
 
 }
